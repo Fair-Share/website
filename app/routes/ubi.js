6c65af8bed942a670fafb503d1aedec949850b2a
go1dfish <justageekbiz@gmail.com>
2015-04-06 17:44:30 -0700

    Don't count deleted users

--- app/routes/ubi.js
+++ app/routes/ubi.js
@@ -4,17 +4,15 @@ import client from 'fairshare-site/client';
 export default Ember.Route.extend({
   model: function() {
     return client('/r/GetFairShare/new').get().then(function(result) {
-      return result.data.children;
+      return (((result||{}).data||{}).children||[]).getEach('data');
     }).then(function(posts) {
-      var post = posts.find(function(post) {
-        console.log('1', post);
-        return post.data.title.match(/Prototype Distribution/);
+      return posts.find(function(post) {
+        return post.title.match(/Prototype Distribution/);
       });
-      if (post) {return post.data};
     }).then(function(post) {
       return client('/r/GetFairShare/comments/' + post.id + '.json').get().then(function(result) {
         post.comments = result[1].data.children.getEach('data');
-        post.beneficiaries = post.comments.getEach('author').uniq().without('PoliticBot');
+        post.beneficiaries = post.comments.getEach('author').uniq().without('PoliticBot').without('[deleted]');
         return post;
       });
     });
