18b560fd1e645557fcba4836d473a7655cb9bc01
go1dfish <justageekbiz@gmail.com>
2015-06-04 17:07:36 -0700

    How hard can it be to get a link right

--- app/services/auth.js
+++ app/services/auth.js
@@ -122,7 +122,7 @@ export default Ember.Service.extend({
     var view = Ember.View.create({template: template});
     view.createElement();
     return [
-      "# [ಥ_ಥ PoliticBot (gets nsfw)]https://modlog.github.io/#/radio?autoplay=true&ytid=c8N72t7aScY)",
+      "# [ಥ_ಥ PoliticBot (gets nsfw)](https://modlog.github.io/#/radio?autoplay=true&ytid=c8N72t7aScY)",
       '---',
       //view.element.innerHTML,
       message,
